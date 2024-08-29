7c983c743ff3666bc2017fd60bb61a117cc30c72
Raphael Cavalcante <133365398+RaphaelsPereira@users.noreply.github.com>
2024-08-29 14:24:23 -0300

    Initial commit

---
+++ NodeBancoDeDados-master/NodeBancoDados.sql
@@ -0,0 +1,25 @@
+/* CREATE DATABASE PresidentesDB  */
+
+/* use PresidentesDB */
+
+/*CREATE TABLE Presidentes (
+id INTEGER,
+presidente VARCHAR(35),
+vice VARCHAR(35),
+ano INTEGER);*/
+
+/*ALTER TABLE Presidentes MODIFY id INT AUTO_INCREMENT PRIMARY KEY;*/
+
+/* INSERT INTO presidentes (presidente, vice, ano) VALUES ('José Sarnei', 'Cargo Vago', 1985); */
+/* INSERT INTO presidentes (presidente, vice, ano) VALUES ('Fernando Collor', 'Itamar Franco', 1990) */
+
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Itamar Franco', 'Fernando Henrique Cardoso', 1992);*/
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Fernando Henrique Cardoso', 'Marco Maciel', 1995);*/
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Luiz Inácio Lula da Silva', 'José Alencar', 2003);*/
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Dilma Rousseff', 'Michel Temer', 2011);*/
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Michel Temer', 'Não tinha vice', 2016);*/
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Jair Bolsonaro', 'Hamilton Mourão', 2019);*/
+/*INSERT INTO Presidentes (presidente, vice, ano) VALUES ('Luiz Inácio Lula da Silva', 'Geraldo Alckmin', 2023);*/
+
+
+
